0489e1100861426b8642dfde0e6f4037ec281c54
Md Tayam Hasan Mahim <mdtayamhasanmahim7531@gmail.com>
2026-05-24 23:51:42 +0600

    second

---
+++ app/middlewares/adminMiddleware.js
@@ -0,0 +1,14 @@
+// middlewares/adminMiddleware.js
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(403).json({
+      success: false,
+      message: "Admin access only",
+    });
+  }
+};
+
+module.exports = admin;
