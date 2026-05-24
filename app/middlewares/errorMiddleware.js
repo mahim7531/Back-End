4ac03582fd4fdb6c85ac5f50d859a87496866718
Md Tayam Hasan Mahim <mdtayamhasanmahim7531@gmail.com>
2026-05-24 23:51:01 +0600

    middlewares files update

---
+++ app/middlewares/errorMiddleware.js
@@ -0,0 +1,11 @@
+// middlewares/errorMiddleware.js
+
+const errorHandler = (err, req, res, next) => {
+  res.status(res.statusCode || 500).json({
+    success: false,
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+module.exports = errorHandler;
